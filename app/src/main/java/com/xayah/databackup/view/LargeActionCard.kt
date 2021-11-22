b8c3a5b8bd00dc7c683b01ea9a65cff0556a6928
Xayah <zds1249475336@gmail.com>
2021-11-22 12:56:15 +0800

    feat: 移植LargeActionCard
    * From https://github.com/Kr328/ClashForAndroid/tree/main/design/src/main

---
+++ app/src/main/java/com/xayah/databackup/view/LargeActionCard.kt
@@ -0,0 +1,73 @@
+package com.xayah.databackup.view
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.AttrRes
+import androidx.annotation.DimenRes
+import com.google.android.material.card.MaterialCardView
+import com.xayah.databackup.R
+import com.xayah.databackup.databinding.ComponentLargeActionLabelBinding
+import com.xayah.databackup.util.resolveClickableAttrs
+import com.xayah.databackup.util.resolveThemedColor
+import com.xayah.databackup.util.selectableItemBackground
+
+class LargeActionCard @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0
+) : MaterialCardView(context, attributeSet, defStyleAttr) {
+    private val binding = ComponentLargeActionLabelBinding
+        .inflate(LayoutInflater.from(context), this, true)
+
+    var text: CharSequence?
+        get() = binding.textView.text
+        set(value) {
+            binding.textView.text = value
+        }
+
+    var subtext: CharSequence?
+        get() = binding.subtextView.text
+        set(value) {
+            binding.subtextView.text = value
+        }
+
+    var icon: Drawable?
+        get() = binding.iconView.background
+        set(value) {
+            binding.iconView.background = value
+        }
+
+    init {
+        context.resolveClickableAttrs(attributeSet, defStyleAttr) {
+            isFocusable = focusable(true)
+            isClickable = clickable(true)
+            foreground = foreground() ?: context.selectableItemBackground
+        }
+
+        context.theme.obtainStyledAttributes(
+            attributeSet,
+            R.styleable.LargeActionCard,
+            defStyleAttr,
+            0
+        ).apply {
+            try {
+                icon = getDrawable(R.styleable.LargeActionCard_icon)
+                text = getString(R.styleable.LargeActionCard_text)
+                subtext = getString(R.styleable.LargeActionCard_subtext)
+            } finally {
+                recycle()
+            }
+        }
+
+        minimumHeight = getPixels(R.dimen.large_action_card_min_height)
+        radius = getPixels(R.dimen.large_action_card_radius).toFloat()
+        elevation = getPixels(R.dimen.large_action_card_elevation).toFloat()
+    }
+
+    private fun getPixels(@DimenRes resId: Int): Int {
+        return resources.getDimensionPixelSize(resId)
+    }
+
+}
