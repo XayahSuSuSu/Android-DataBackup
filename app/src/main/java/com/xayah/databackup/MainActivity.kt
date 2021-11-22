7e5cb4d939ca3cf2b73208db722ca8298a618a9d
Xayah <zds1249475336@gmail.com>
2021-11-23 01:18:46 +0800

    feat: 添加Root检测LargeActionCard
    * 使用DataBinding绑定数据
    
    * 使用Libsu检测权限

--- app/src/main/java/com/xayah/databackup/MainActivity.kt
+++ app/src/main/java/com/xayah/databackup/MainActivity.kt
@@ -2,13 +2,24 @@ package com.xayah.databackup
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.topjohnwu.superuser.Shell
+import com.xayah.databackup.databinding.ActivityMainBinding
 import com.xayah.databackup.util.WindowUtil
 import com.xayah.databackup.util.resolveThemedBoolean
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         WindowUtil.setWindowMode(!resolveThemedBoolean(android.R.attr.windowLightStatusBar), window)
+
+        binding()
+    }
+
+    private fun binding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.isRoot = Shell.getShell().isRoot
     }
 }
