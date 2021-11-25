f7e702065df78de8a4cf71f641bfec03b273d4cf
Xayah <zds1249475336@gmail.com>
2021-11-25 13:34:10 +0800

    feat: SelectActivity添加标题栏按钮
    * 处理NameNotFoundException异常

--- app/src/main/java/com/xayah/databackup/SelectActivity.kt
+++ app/src/main/java/com/xayah/databackup/SelectActivity.kt
@@ -1,13 +1,16 @@
 package com.xayah.databackup
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.xayah.databackup.adapter.AppListAdapter
-import com.xayah.databackup.model.AppInfo
 import com.xayah.databackup.databinding.ActivitySelectBinding
+import com.xayah.databackup.model.AppInfo
 import com.xayah.databackup.util.DataUtil
 import com.xayah.databackup.util.ShellUtil
 import com.xayah.databackup.util.WindowUtil
@@ -18,6 +21,7 @@ import kotlinx.coroutines.launch
 class SelectActivity : AppCompatActivity() {
     lateinit var mContext: Context
     lateinit var binding: ActivitySelectBinding
+    lateinit var adapter: AppListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
@@ -29,12 +33,18 @@ class SelectActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.select_apps_menu, menu)
+        return true
+    }
+    
     private fun binding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_select)
     }
 
     private fun init() {
-        val adapter = AppListAdapter(this)
+        adapter = AppListAdapter(this)
         val mShell = ShellUtil(this)
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerViewAppList.layoutManager = layoutManager
@@ -43,15 +53,19 @@ class SelectActivity : AppCompatActivity() {
             for (i in appPackages) {
                 val app = i.split(" ")[0]
                 val packageName = i.split(" ")[1]
-                val (appIcon, appName, appPackage) = DataUtil.getAppInfo(mContext, packageName)
-                val appInfo = AppInfo(
-                    appIcon,
-                    appName,
-                    appPackage,
-                    app.contains("#"),
-                    app.contains("!")
-                )
-                adapter.addApp(appInfo)
+                try {
+                    val (appIcon, appName, appPackage) = DataUtil.getAppInfo(mContext, packageName)
+                    val appInfo = AppInfo(
+                        appIcon,
+                        appName,
+                        appPackage,
+                        app.contains("#"),
+                        app.contains("!")
+                    )
+                    adapter.addApp(appInfo)
+                } catch (e: PackageManager.NameNotFoundException) {
+                    e.printStackTrace()
+                }
             }
             runOnUiThread {
                 binding.recyclerViewAppList.adapter = adapter
