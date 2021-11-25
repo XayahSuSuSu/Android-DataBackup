f553d9a5daf183228c269158bff9678a44df532d
Xayah <zds1249475336@gmail.com>
2021-11-25 14:39:02 +0800

    feat: 添加SelectActivity跳转
    * 启动时检测并释放脚本资源

--- app/src/main/java/com/xayah/databackup/MainActivity.kt
+++ app/src/main/java/com/xayah/databackup/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.xayah.databackup
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.topjohnwu.superuser.Shell
 import com.xayah.databackup.databinding.ActivityMainBinding
+import com.xayah.databackup.util.ShellUtil
 import com.xayah.databackup.util.WindowUtil
 import com.xayah.databackup.util.resolveThemedBoolean
 
@@ -16,10 +18,20 @@ class MainActivity : AppCompatActivity() {
         WindowUtil.setWindowMode(!resolveThemedBoolean(android.R.attr.windowLightStatusBar), window)
 
         binding()
+        init()
     }
 
     private fun binding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.isRoot = Shell.getShell().isRoot
+        binding.largeActionCardSelectApps.setOnClickListener {
+            val intent = Intent(this, SelectActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun init() {
+        val mShell = ShellUtil(this)
+        mShell.extractAssets()
     }
 }
