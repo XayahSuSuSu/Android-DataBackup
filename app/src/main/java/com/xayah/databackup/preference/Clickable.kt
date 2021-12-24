036d3c031b17b6bf744ce5587ab3b4cb35e2611a
Xayah <zds1249475336@gmail.com>
2021-12-24 17:37:01 +0800

    feat: 移植Preference - EditableText & Clickable
    * From https://github.com/Kr328/ClashForAndroid/tree/main/design/src/main

---
+++ app/src/main/java/com/xayah/databackup/preference/Clickable.kt
@@ -0,0 +1,73 @@
+package com.xayah.databackup.preference
+
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.View
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+import com.xayah.databackup.databinding.PreferenceClickableBinding
+
+interface ClickablePreference : Preference {
+    var title: CharSequence
+
+    var icon: Drawable?
+    var summary: CharSequence?
+
+    fun clicked(clicked: () -> Unit)
+}
+
+fun PreferenceScreen.clickable(
+    @StringRes title: Int,
+    @DrawableRes icon: Int? = null,
+    @StringRes summary: Int? = null,
+    configure: ClickablePreference.() -> Unit = {}
+): ClickablePreference {
+    val binding = PreferenceClickableBinding
+        .inflate(LayoutInflater.from(context), root, false)
+
+    val impl = object : ClickablePreference {
+        override var icon: Drawable?
+            get() = binding.iconView.background
+            set(value) {
+                binding.iconView.background = value
+            }
+        override var title: CharSequence
+            get() = binding.titleView.text
+            set(value) {
+                binding.titleView.text = value
+            }
+        override var summary: CharSequence?
+            get() = binding.summaryView.text
+            set(value) {
+                binding.summaryView.text = value
+                binding.summaryView.visibility = if (value == null) View.GONE else View.VISIBLE
+            }
+        override val view: View
+            get() = binding.root
+
+        override fun clicked(clicked: () -> Unit) {
+            binding.root.setOnClickListener {
+                clicked()
+            }
+        }
+    }
+
+    impl.title = context.getText(title)
+
+    if (icon != null) {
+        impl.icon = ContextCompat.getDrawable(context, icon)
+    }
+
+    if (summary != null) {
+        impl.summary = context.getText(summary)
+    } else {
+        impl.summary = null
+    }
+
+    impl.configure()
+
+    addElement(impl)
+
+    return impl
+}
---
+++ app/src/main/java/com/xayah/databackup/preference/EditableText.kt
@@ -0,0 +1,92 @@
+package com.xayah.databackup.preference
+
+import android.app.Activity
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.xayah.databackup.R
+import com.xayah.databackup.databinding.DialogTextFieldBinding
+
+interface EditableTextPreference : ClickablePreference {
+    var placeholder: CharSequence?
+    var empty: CharSequence?
+    var text: String?
+}
+
+fun PreferenceScreen.editableText(
+    onCreated: (DialogTextFieldBinding) -> Unit = {},
+    onPositiveEvent: (DialogTextFieldBinding) -> Unit = {},
+    onNegativeEvent: (DialogTextFieldBinding) -> Unit = {},
+    onNeutralEvent: (DialogTextFieldBinding) -> Unit = {},
+    @StringRes title: Int,
+    summary: CharSequence? = null,
+    @DrawableRes icon: Int? = null,
+    @StringRes placeholder: Int? = null,
+    @StringRes empty: Int? = null,
+    configure: EditableTextPreference.() -> Unit = {},
+): EditableTextPreference {
+    val impl = object : EditableTextPreference, ClickablePreference by clickable(title, icon) {
+        override var placeholder: CharSequence? = null
+        override var empty: CharSequence? = null
+        override var text: String? = null
+            set(value) {
+                field = value
+
+                when {
+                    value == null -> {
+                        this.summary = this.placeholder
+                    }
+                    value.isEmpty() -> {
+                        this.summary = this.empty
+                    }
+                    else -> {
+                        this.summary = value
+                    }
+                }
+            }
+    }
+    var binding: DialogTextFieldBinding
+
+    impl.clicked {
+        binding = DialogTextFieldBinding
+            .inflate((context as Activity).layoutInflater, root, false)
+        binding.textLayout.hint = context.getString(title)
+        MaterialAlertDialogBuilder(context)
+            .setTitle(title)
+            .setView(binding.root)
+            .setCancelable(true)
+            .setPositiveButton(context.getString(R.string.dialog_positive)) { dialog, _ ->
+                onPositiveEvent(
+                    binding
+                )
+                impl.summary = binding.textField.text.toString()
+                dialog.dismiss()
+            }
+            .setNegativeButton(context.getString(R.string.dialog_negative)) { dialog, _ ->
+                onNegativeEvent(
+                    binding
+                )
+                dialog.dismiss()
+            }
+            .setNeutralButton(context.getString(R.string.dialog_neutral)) { dialog, _ ->
+                onNeutralEvent(
+                    binding
+                )
+                impl.summary = binding.textField.text.toString()
+                dialog.dismiss()
+            }
+            .show()
+        onCreated(binding)
+    }
+
+    if (placeholder != null) {
+        impl.placeholder = context.getText(placeholder)
+    }
+
+    if (empty != null) {
+        impl.empty = context.getText(empty)
+    }
+    impl.summary = summary
+    impl.configure()
+    return impl
+}
