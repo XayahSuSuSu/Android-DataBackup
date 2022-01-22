dafab748982a8b3ca3cc6a8505e2f3853c308ae4
Xayah <zds1249475336@gmail.com>
2022-01-23 00:47:59 +0800

    fix: 修复未安装应用获取失败导致的闪退

--- app/src/main/java/com/xayah/databackup/util/DataUtil.kt
+++ app/src/main/java/com/xayah/databackup/util/DataUtil.kt
@@ -3,18 +3,26 @@ package com.xayah.databackup.util
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import androidx.appcompat.content.res.AppCompatResources
+import com.xayah.databackup.R
 import java.text.SimpleDateFormat
 import java.util.*
 
 class DataUtil {
     companion object {
         fun getAppInfo(mContext: Context, packageName: String): Triple<Drawable, String, String> {
-            val packageManager: PackageManager = mContext.packageManager
-            val appInfo =
-                packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
-            val appName = packageManager.getApplicationLabel(appInfo)
-            val appIcon = packageManager.getApplicationIcon(appInfo)
-            return Triple(appIcon, appName.toString(), packageName)
+            var appName = mContext.getString(R.string.get_error)
+            var appIcon = AppCompatResources.getDrawable(mContext, R.drawable.ic_outline_android)!!
+            try {
+                val packageManager: PackageManager = mContext.packageManager
+                val appInfo =
+                    packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+                appName = packageManager.getApplicationLabel(appInfo).toString()
+                appIcon = packageManager.getApplicationIcon(appInfo)
+            } catch (e: PackageManager.NameNotFoundException) {
+                e.printStackTrace()
+            }
+            return Triple(appIcon, appName, packageName)
         }
 
         fun getFormatDate(timestamp: Long): String {
