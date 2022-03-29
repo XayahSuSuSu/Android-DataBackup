86125016e937339353f4b4aed7fc34d412eca746
Xayah <zds1249475336@gmail.com>
2022-03-29 10:22:09 +0800

    feat: 添加全局入口并初始化

---
+++ app/src/main/java/com/xayah/databackup/App.kt
@@ -0,0 +1,47 @@
+package com.xayah.databackup
+
+import android.app.Application
+import android.content.Context
+import com.topjohnwu.superuser.Shell
+import com.xayah.databackup.util.Command
+import com.xayah.databackup.util.Path
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class App : Application() {
+    companion object {
+        init {
+            Shell.enableVerboseLogging = BuildConfig.DEBUG;
+            Shell.setDefaultBuilder(
+                Shell.Builder.create()
+                    .setFlags(Shell.FLAG_MOUNT_MASTER or Shell.FLAG_REDIRECT_STDERR)
+                    .setTimeout(10)
+                    .setInitializers(EnvInitializer::class.java)
+            )
+        }
+    }
+
+    class EnvInitializer : Shell.Initializer() {
+        override fun onInit(context: Context, shell: Shell): Boolean {
+            shell.newJob()
+                .add("export PATH=${Path.getExternalFilesDir(context)}/bin:\$PATH")
+                .exec()
+            return true
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val that = this
+        CoroutineScope(Dispatchers.IO).launch {
+            if (!Command.ls("${Path.getExternalFilesDir(that)}/bin")) {
+                Command.extractAssets(that, "${Command.getABI()}/bin.zip", "bin.zip")
+                Command.unzip(
+                    "${Path.getExternalFilesDir(that)}/bin.zip",
+                    "${Path.getExternalFilesDir(that)}/bin"
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/xayah/databackup/util/Command.kt
+++ app/src/main/java/com/xayah/databackup/util/Command.kt
@@ -65,5 +65,17 @@ class Command {
             }
             return ""
         }
+
+        fun ls(path: String): Boolean {
+            return Shell.cmd("ls -i $path").exec().isSuccess
+        }
+
+        fun mkdir(path: String): Boolean {
+            return Shell.cmd("mkdir -p $path").exec().isSuccess
+        }
+
+        fun unzip(filePath: String, outPath: String) {
+            if (mkdir(outPath)) Shell.cmd("unzip $filePath -d $outPath").exec()
+        }
     }
 }
