ae87a7081faf7edd3209d47f8febf11066e0400e
Xayah <zds1249475336@gmail.com>
2022-03-30 11:32:07 +0800

    feat: 优化BackupFragment RecyclerView第一项margin

--- app/src/main/java/com/xayah/databackup/adapter/AppListAdapter.kt
+++ app/src/main/java/com/xayah/databackup/adapter/AppListAdapter.kt
@@ -3,10 +3,12 @@ package com.xayah.databackup.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.drakeet.multitype.ItemViewDelegate
 import com.xayah.databackup.databinding.AdapterAppListBinding
 import com.xayah.databackup.model.AppInfo
+import com.xayah.databackup.util.dp
 
 class AppListAdapter : ItemViewDelegate<AppInfo, AppListAdapter.ViewHolder>() {
 
@@ -25,6 +27,21 @@ class AppListAdapter : ItemViewDelegate<AppInfo, AppListAdapter.ViewHolder>() {
         binding.appIcon.setImageDrawable(item.icon)
         binding.appName.text = item.appName
         binding.appPackage.text = item.packageName
+        if (holder.bindingAdapterPosition == 0) {
+            binding.materialCardView.apply {
+                layoutParams =
+                    LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+                        .apply {
+                            topMargin = 16.dp
+                            bottomMargin = 16.dp
+                            marginStart = 20.dp
+                            marginEnd = 20.dp
+                        }
+            }
+        }
     }
 
     class ViewHolder(val binding: AdapterAppListBinding) : RecyclerView.ViewHolder(binding.root)
