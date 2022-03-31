6ca86139bd9dbbe572b806286a54c5806327124c
Xayah <zds1249475336@gmail.com>
2022-03-31 18:02:33 +0800

    feat: 引入SharedPreferences工具

--- app/src/main/java/com/xayah/databackup/util/Context.kt
+++ app/src/main/java/com/xayah/databackup/util/Context.kt
@@ -1,7 +1,10 @@
 package com.xayah.databackup.util
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import android.content.res.Resources
 import android.util.TypedValue
+
 
 val Int.dp: Int
     get() = TypedValue.applyDimension(
@@ -9,3 +12,16 @@ val Int.dp: Int
         this.toFloat(),
         Resources.getSystem().displayMetrics
     ).toInt()
+
+fun Context.savePreferences(key: String, value: String) {
+    getSharedPreferences("settings", MODE_PRIVATE).edit().apply {
+        putString(key, value)
+        apply()
+    }
+}
+
+fun Context.readPreferences(key: String): String? {
+    getSharedPreferences("settings", MODE_PRIVATE).apply {
+        return getString(key, null)
+    }
+}
