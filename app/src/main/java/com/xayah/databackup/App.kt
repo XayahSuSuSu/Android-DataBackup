5fd7915eb556c0e0f1b280663e3a8b14fb912bc2
Xayah <zds1249475336@gmail.com>
2023-01-29 22:11:56 +0800

    feat: 关闭libsu日志输出
    Change-Id: I055b87fbcda03264ada319f1f39c78e30b9805b2

--- app/src/main/java/com/xayah/databackup/App.kt
+++ app/src/main/java/com/xayah/databackup/App.kt
@@ -12,7 +12,7 @@ import java.io.InputStream
 class App : Application() {
     companion object {
         init {
-            Shell.enableVerboseLogging = BuildConfig.DEBUG
+            Shell.enableVerboseLogging = false
             Shell.setDefaultBuilder(
                 Shell.Builder.create()
                     .setFlags(Shell.FLAG_MOUNT_MASTER or Shell.FLAG_REDIRECT_STDERR)
