03b05e8b4da8286c972b8c2e552522b7be23e8bf
Xayah <zds1249475336@gmail.com>
2023-02-24 01:04:14 +0800

    fix: 修复ManifestDesc深色模式Bug
    Change-Id: Id90e14897cc48eb5d57017c934d4258c94bf10f7

--- app/src/main/java/com/xayah/databackup/ui/activity/list/components/ManifestDesc.kt
+++ app/src/main/java/com/xayah/databackup/ui/activity/list/components/ManifestDesc.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -37,11 +38,11 @@ fun ManifestDesc(
             contentDescription = null,
         )
         Column {
-            androidx.compose.material.Text(
+            Text(
                 text = title,
                 style = MaterialTheme.typography.titleSmall,
             )
-            androidx.compose.material.Text(
+            Text(
                 text = subtitle,
                 style = MaterialTheme.typography.bodySmall,
             )
