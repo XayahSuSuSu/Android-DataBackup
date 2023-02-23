3b237f745c52fcf40e7d4bbdf26282a0a190d4f8
Xayah <zds1249475336@gmail.com>
2023-02-24 01:01:24 +0800

    fix: 暂时使用Dialog替代ModalBottomSheet
    Change-Id: I3aabf9cef43c78bca8c34b9ce7bda440e8d4fc5e

--- app/src/main/java/com/xayah/databackup/ui/components/BottomSheet.kt
+++ app/src/main/java/com/xayah/databackup/ui/components/BottomSheet.kt
@@ -1,23 +1,41 @@
 package com.xayah.databackup.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.rememberSheetState
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.window.Dialog
+import com.xayah.databackup.R
 
 @ExperimentalMaterial3Api
 @Composable
-fun BottomSheet(isOpen: MutableState<Boolean>, content: @Composable() (ColumnScope.() -> Unit)) {
-    val bottomSheetState = rememberSheetState()
-
+fun BottomSheet(isOpen: MutableState<Boolean>, content: @Composable (ColumnScope.() -> Unit)) {
     if (isOpen.value) {
-        ModalBottomSheet(
-            onDismissRequest = { isOpen.value = false },
-            sheetState = bottomSheetState,
-        ) {
-            content()
+        Dialog(onDismissRequest = { isOpen.value = false }) {
+            val nonePadding = dimensionResource(R.dimen.padding_none)
+            val mediumPadding = dimensionResource(R.dimen.padding_medium)
+            Surface(
+                shape = AlertDialogDefaults.shape,
+                color = AlertDialogDefaults.containerColor,
+                tonalElevation = AlertDialogDefaults.TonalElevation,
+            ) {
+                Column(
+                    modifier = Modifier.padding(
+                        nonePadding,
+                        mediumPadding,
+                        nonePadding,
+                        nonePadding
+                    )
+                ) {
+                    content()
+                }
+            }
         }
     }
 }
