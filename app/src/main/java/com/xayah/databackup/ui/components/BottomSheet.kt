d2744801b7913f3420a13064c2c1f5f75ef9204d
Xayah <zds1249475336@gmail.com>
2023-03-02 16:29:20 +0800

    feat: Bring back ModalBottomSheet
    Change-Id: Ia740f023467124a64d0b20fd9b7d2806c0b6bbd8

--- app/src/main/java/com/xayah/databackup/ui/components/BottomSheet.kt
+++ app/src/main/java/com/xayah/databackup/ui/components/BottomSheet.kt
@@ -1,35 +1,22 @@
 package com.xayah.databackup.ui.components
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Surface
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.window.Dialog
-import com.xayah.databackup.R
 
 @ExperimentalMaterial3Api
 @Composable
 fun BottomSheet(isOpen: MutableState<Boolean>, content: @Composable (ColumnScope.() -> Unit)) {
     if (isOpen.value) {
-        Dialog(onDismissRequest = { isOpen.value = false }) {
-            val mediumPadding = dimensionResource(R.dimen.padding_medium)
-            Surface(
-                shape = AlertDialogDefaults.shape,
-                color = AlertDialogDefaults.containerColor,
-                tonalElevation = AlertDialogDefaults.TonalElevation,
-            ) {
-                Column(
-                    modifier = Modifier.paddingTop(mediumPadding)
-                ) {
-                    content()
-                }
-            }
+        val bottomSheetState = rememberSheetState()
+        ModalBottomSheet(
+            onDismissRequest = { isOpen.value = false },
+            sheetState = bottomSheetState,
+        ) {
+            content()
         }
     }
 }
