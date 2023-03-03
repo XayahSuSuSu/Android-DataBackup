1af6700d682892d561fc9ed1f16641f5647c87cd
Xayah <zds1249475336@gmail.com>
2023-03-04 00:44:49 +0800

    fix: Pass MainViewModel().isRemoteFileInitialized directly
    Change-Id: I3443f8530bed8b014a73124ed3e3770761c06260

--- app/src/main/java/com/xayah/databackup/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/xayah/databackup/ui/activity/main/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.remember
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -37,10 +36,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             DataBackupTheme {
-                val isInitialized = remember {
-                    viewModel.isRemoteFileInitialized
-                }
-                MainScaffold(isInitialized)
+                MainScaffold(viewModel.isRemoteFileInitialized)
             }
         }
     }
