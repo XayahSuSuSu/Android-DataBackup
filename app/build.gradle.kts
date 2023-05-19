336e072d47ac51cde9b3ef4e9b89a29e5b7a052b
Xayah <zds1249475336@gmail.com>
2023-05-19 15:43:11 +0800

    gradle: Implement okhttp
    Change-Id: I0bea3d603f2f5b3cb1fcebd2050ea9e9af9d6f2d

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,9 @@ dependencies {
     // Compose Navigation
     implementation("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
 
 kapt {
