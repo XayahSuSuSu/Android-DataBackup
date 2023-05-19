865c7ce3b7f887f53cd8e005ab04fde356654aa4
Xayah <zds1249475336@gmail.com>
2023-05-19 15:40:11 +0800

    gradle: Implement appcompat
    Change-Id: I2b97495bbd2e1e7ddf280503ef45cdb8a485d3e3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.1")
     implementation(platform("androidx.compose:compose-bom:2023.05.01"))
