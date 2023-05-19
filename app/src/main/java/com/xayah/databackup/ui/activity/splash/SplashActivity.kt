938967b387d0634b42b3bb563d835b8c0c986d8b
Xayah <zds1249475336@gmail.com>
2023-05-19 15:42:10 +0800

    feat: Implement splashscreen
    Change-Id: Ifb582b4afd10d87e1c88678cd13d3ab0f8acab3c

---
+++ app/src/main/java/com/xayah/databackup/ui/activity/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.xayah.databackup.ui.activity.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.xayah.databackup.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        installSplashScreen()
+
+        // Workaround for HarmonyOS
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
