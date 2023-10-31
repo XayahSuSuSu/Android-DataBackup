9fb6d4b1cff678818b18fc60d2fb459f474df5cf
Xayah <zds1249475336@gmail.com>
2023-10-31 23:09:32 +0800

    perf: Set WindowInsets for ModalBottomSheet()
    Change-Id: I254c56a88854fb9c2ebc29102a5c025be9c82d67

--- app/src/main/java/com/xayah/databackup/ui/components/BottomSheet.kt
+++ app/src/main/java/com/xayah/databackup/ui/components/BottomSheet.kt
@@ -1,11 +1,13 @@
 package com.xayah.databackup.ui.components
 
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.unit.dp
 
 @ExperimentalMaterial3Api
 @Composable
@@ -15,6 +17,7 @@ fun BottomSheet(isOpen: MutableState<Boolean>, content: @Composable (ColumnScope
         ModalBottomSheet(
             onDismissRequest = { isOpen.value = false },
             sheetState = bottomSheetState,
+            windowInsets = WindowInsets(0.dp, 0.dp, 0.dp, 0.dp)
         ) {
             content()
         }
