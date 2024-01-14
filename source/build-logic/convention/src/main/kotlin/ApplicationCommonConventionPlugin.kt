2ca6d3461a3d7b6f70a974aee0edc4f582f746d1
Xayah <zds1249475336@gmail.com>
2024-01-14 14:33:20 +0800

    gradle: Do not shrink on debug build
    Change-Id: Ie05e2e126a3938f2da0f1d526070231809982145

--- source/build-logic/convention/src/main/kotlin/ApplicationCommonConventionPlugin.kt
+++ source/build-logic/convention/src/main/kotlin/ApplicationCommonConventionPlugin.kt
@@ -19,8 +19,8 @@ private fun Project.configureCommon() {
                 buildConfigField("Boolean", "ENABLE_VERBOSE", "false")
             }
             debug {
-                isMinifyEnabled = true
-                isShrinkResources = true
+                isMinifyEnabled = false
+                isShrinkResources = false
                 proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
                 buildConfigField("Boolean", "ENABLE_VERBOSE", "true")
             }
