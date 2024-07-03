190a6236db7f36b385448b6fccc75dbecc27e659
frknkrc44 <krc440002@gmail.com>
2024-07-03 18:28:11 +0300

    fix: Filter /data/media entries

--- source/core/data/src/main/kotlin/com/xayah/core/data/repository/DirectoryRepository.kt
+++ source/core/data/src/main/kotlin/com/xayah/core/data/repository/DirectoryRepository.kt
@@ -83,6 +83,7 @@ class DirectoryRepository @Inject constructor(
 
             // Internal storage
             val internalList = rootService.listFilePaths(PathUtil.getDataMediaDir(), listFiles = false)
+                .filter { it.substring(it.lastIndexOf("/")+1).toIntOrNull() != null } // just select 0 10 999 etc.
             val internalDirs = mutableListOf<DirectoryUpsertEntity>()
             for (storageItem in internalList) {
                 // e.g. /data/media/0
